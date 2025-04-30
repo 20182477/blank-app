5fc02c734f70d2abd5054f796d0b25906af0061c
20182477 <manorqaz@gmail.com>
2025-04-30 09:24:19 +0900

    Update kpro_homework_2.py

--- kpro_homework_2.py
+++ kpro_homework_2.py
@@ -11,7 +11,7 @@ from sklearn.metrics import r2_score
 import matplotlib.font_manager as fm
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
-FONT_PATH = os.path.join(script_dir, 'NanumGothic(1).ttf')
+FONT_PATH = os.path.join(script_dir, 'NanumGothic (1).ttf')
 fm.fontManager.addfont(FONT_PATH)
 font_name = fm.FontProperties(fname=FONT_PATH).get_name()
 plt.rcParams['font.family'] = font_name
